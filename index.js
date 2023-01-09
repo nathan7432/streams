d3a2e37973214c45689be897f938c9e08cfc1e45
nathan7432 <steelersfan5052@yahoo.com>
2023-01-09 09:53:03 -0500

    bigger array

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const big = []
 
 while (true) {
-  big.push(new Array(1000000).join('x'))
+  big.push(new Array(1000000000).join('x'))
   console.log(`Heap allocated ${Math.round(process.memoryUsage()['heapUsed']/1024/1024/1024*100)/100} GB`);
   console.log(big.length)
 }
