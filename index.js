64bd9e0524c46c7a83b5d0187c199832362c80ad
nathan7432 <steelersfan5052@yahoo.com>
2023-01-09 11:55:08 -0500

    demo1 complete

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const big = []
+const cliProgress = require('cli-progress');
+const big = [];
+let space = 0;
+const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+bar1.start(250, 0);
 
 while (true) {
-  big.push(new Array(1000000000).join('x'))
-  console.log(`Heap allocated ${Math.round(process.memoryUsage()['heapUsed']/1024/1024/1024*100)/100} GB`);
-  console.log(big.length)
+  big.push(new Array(100000000).join('x'))
+  space = Math.round(process.memoryUsage()['heapUsed']/1024/1024*100)/100
+  if (space % 1 === 0)
+    bar1.update(space);
 }
